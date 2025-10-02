80b3c73cb1e661e3391d6ad2f74b7936ad8b497b
Kyrylo Prylipko <lambnelsen@gmail.com>
2025-10-02 15:37:35 +0200

    user space - app init

--- run.sh
+++ run.sh
@@ -7,10 +7,18 @@ QEMU=qemu-system-riscv32
 CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
 
+# Path to llvm-objcopy
+OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
+
+# Build the shell (application)
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
 # Build the kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c common.c
+    kernel.c common.c shell.bin.o
 
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
-    -kernel kernel.elf 
+    -kernel kernel.elf
