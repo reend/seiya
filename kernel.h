6b4e2768b373636d8f7948914a474d9dc79f5593
Kyrylo Prylipko <lambnelsen@gmail.com>
2025-10-02 10:46:15 +0200

    process control block, process switching

--- kernel.h
+++ kernel.h
@@ -1,6 +1,18 @@
 #pragma once
 #include "common.h"
 
+#define PROCS_MAX 8       // Maximum number of processes
+
+#define PROC_UNUSED   0   // Unused process control structure
+#define PROC_RUNNABLE 1   // Runnable process
+
+struct process {
+    int pid;             // Process ID
+    int state;           // Process state: PROC_UNUSED or PROC_RUNNABLE
+    vaddr_t sp;          // Stack pointer
+    uint8_t stack[8192]; // Kernel stack
+};
+
 struct trap_frame {
     uint32_t ra;
     uint32_t gp;
